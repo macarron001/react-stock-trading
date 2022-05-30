0f38740846a0dfc3dd13b1cd9226ca6f86600582
Ronny <91857581+macarron001@users.noreply.github.com>
2022-05-30 21:46:10 +0800

    added content and style to Portfolio component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./index.css"
 import Register from "./components/Register"
 import Login from "./components/Login"
 import Navbar from "./components/navbar/Navbar"
+import Stocks from './components/trader/Stocks'
+import Portfolio from './components/trader/Portfolio'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Portfolio />
     </Router>
   );
 }
---
+++ src/components/trader/Portfolio.jsx
@@ -0,0 +1,95 @@
+
+
+const Portfolio = () => {
+  const stocks = [
+    {
+      symbol : 'NVDA',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'BLH',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRW',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRA',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRB',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRC',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRD',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRE',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRF',
+      company_name : 'HEHE',
+      price : 2.00,
+    },
+    {
+      symbol : 'GRG',
+      company_name : 'HEHE',
+      price : 2.00,
+    }
+  ]
+
+  return (
+    <div className="wrapper-portfolio">
+      <div className="container-portfolio">
+        <table>
+            <thead className="thead-portfolio">
+              <tr>
+                <th>Stock</th>
+                <th>Shares</th>
+                <th>Total</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody className="portfolio-table-body">
+              {stocks !== "" && stocks.map((stock) => {
+                console.log(stock.symbol);
+                return (
+                  <tr>
+                    <td>{stock.symbol}</td>
+                    <td>{stock.company_name}</td>
+                    <td>{stock.price}</td>
+                    <td><button className='portfolio-sell'>Sell</button></td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+      </div>
+      <div className="portfolio-footer">
+        <div className="balance">
+          <p>PORTFOLIO TOTAL: <span className="first">P10,000.00</span> </p>
+          <p>BALANCE: <span className="last">P10,000.00</span></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
